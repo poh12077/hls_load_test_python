f18f84abdcb2affbe3bff39a53d61c8557b0552a
yh <dudghk1207@gmail.com>
2022-03-31 18:19:13 +0900

    nana

--- hls_load_test_concurrent_user.py
+++ hls_load_test_concurrent_user.py
@@ -1,16 +1,17 @@
 import http.client
 import threading
+import os
 
-thread_num=1
+thread_num=3
 #host = "10.0.4.101:8080"
 host = "192.168.0.124:1935"
 socket_num=5
 base_url = "/vod/mp4:sample.mp4/"
 #base_url = "/B120156699_EPI0001_02_t33.mp4/"
 
+
 def request_ts(id):
-    # session_id=0
-    # session_max = 1000
+    session_id=0
     body = None
     headers = {"Host": "origin.media.com"}
 
@@ -24,10 +25,11 @@ def request_ts(id):
         conn_list.append(http.client.HTTPConnection(host))
         conn_list[i].request("GET", base_url + "playlist.m3u8" , body, headers)
         res_list.append( conn_list[i].getresponse() )
-        m3u8 = res_list[i].read()
+        if i!=0:
+            res_list[i].read()
 
 
-    #m3u8 = res_list[0].read()
+    m3u8 = res_list[0].read()
     m3u8 = m3u8.decode('utf-8')
     m3u8=m3u8.split('\n')[-2]
 
@@ -51,21 +53,20 @@ def request_ts(id):
 
     while True:
         for i in ts:
-            #headers = {"user-agent": str(session_id)}
+            # headers = {"user-agent": str(session_id)}
             # conn.request('GET', i, body, headers )
             # res = conn.getresponse()
             # res.read()
 
             for j in range(0,socket_num):
+               # session_id = str(os.getpid()) + ' ' + str(j)
+                session_id = str( threading.get_ident() ) + ' ' + str(j)
+                headers = {"Host": "origin.media.com", "user-agent": session_id }
                 conn_list[j].request('GET', i, body, headers )
                 res_list[j] = conn_list[j].getresponse() 
                 res_list[j].read()
-                print( str(id) +' ' + str(res_list[j].status) + ' ' + i)
-            # session_id +=1
-            # if session_id == session_max:
-            #     session_id = 0
+                print( session_id +' ' + str(res_list[j].status) + ' ' + i)
             
-            #print( str(id) +' ' + str(res.status) + ' ' + i)
 
 
 
